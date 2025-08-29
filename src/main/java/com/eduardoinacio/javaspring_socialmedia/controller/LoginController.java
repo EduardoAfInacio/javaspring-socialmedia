fe6044dcd78b686ab870f87fbd2e7602049a3afe
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-29 19:07:02 -0300

    Finishing login method inside LoginController.

---
+++ src/main/java/com/eduardoinacio/javaspring_socialmedia/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.eduardoinacio.javaspring_socialmedia.controller;
+
+import com.eduardoinacio.javaspring_socialmedia.controller.dto.LoginRequest;
+import com.eduardoinacio.javaspring_socialmedia.controller.dto.LoginResponse;
+import com.eduardoinacio.javaspring_socialmedia.service.LoginService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    private JwtEncoder jwtEncoder;
+    private LoginService loginService;
+
+    public LoginController(JwtEncoder jwtEncoder, LoginService loginService){
+        this.jwtEncoder = jwtEncoder;
+        this.loginService = loginService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
+        LoginResponse response = loginService.login(request.email(), request.password());
+        return ResponseEntity.ok(response);
+    }
+}
