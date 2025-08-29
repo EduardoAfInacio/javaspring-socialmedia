06784b404d30b02f70a294cfd336e7d6c19d02c5
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-29 15:58:19 -0300

    Updating Role entity.

--- src/main/java/com/eduardoinacio/javaspring_socialmedia/entity/Role.java
+++ src/main/java/com/eduardoinacio/javaspring_socialmedia/entity/Role.java
@@ -3,7 +3,6 @@ package com.eduardoinacio.javaspring_socialmedia.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Value;
 
 @Entity
 @Table(name = "tb_roles")
