b86307ecf06e858f7ece52cf8e3f5fc23fcbd499
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-29 15:03:03 -0300

    first commit

---
+++ src/main/java/com/eduardoinacio/javaspring_socialmedia/JavaspringSocialmediaApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoinacio.javaspring_socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringSocialmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringSocialmediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardoinacio/javaspring_socialmedia/JavaspringSocialmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoinacio.javaspring_socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringSocialmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
