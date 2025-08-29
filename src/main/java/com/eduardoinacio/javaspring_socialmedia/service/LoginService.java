c1a5e1da66f613720f92d0a4f7ff3ad86e9d28fa
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-29 19:06:43 -0300

    Creating login method inside LoginService.

--- src/main/java/com/eduardoinacio/javaspring_socialmedia/service/LoginService.java
+++ src/main/java/com/eduardoinacio/javaspring_socialmedia/service/LoginService.java
@@ -1,13 +1,49 @@
 package com.eduardoinacio.javaspring_socialmedia.service;
 
+
+import com.eduardoinacio.javaspring_socialmedia.controller.dto.LoginResponse;
 import com.eduardoinacio.javaspring_socialmedia.repository.UserRepository;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class LoginService {
     private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+    private JwtEncoder jwtEncoder;
 
-    public LoginService(UserRepository userRepository) {
+    public LoginService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, JwtEncoder jwtEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    public LoginResponse login(String email, String password){
+        var user = userRepository.findByEmail(email);
+
+        if(user.isEmpty() || !user.get().passwordVerification(password, passwordEncoder)){
+            throw new BadCredentialsException("Email/Password invalid or user does not exist");
+        }
+
+        var now = Instant.now();
+        var expiresIn = now.plusSeconds(900);
+
+        var claims = JwtClaimsSet
+                .builder()
+                .issuer("${spring.application.name}")
+                .subject(user.get().getName())
+                .issuedAt(now)
+                .expiresAt(expiresIn)
+                .build();
+
+        var tokenEncoded = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return new LoginResponse(tokenEncoded, expiresIn.getEpochSecond());
     }
 }
