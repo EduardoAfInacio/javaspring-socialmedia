8f2bd8591fba66a4248aa5900f275ddcbabeb58e
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-30 15:50:29 -0300

    Creating UserController and listAll users method.

---
+++ src/main/java/com/eduardoinacio/javaspring_socialmedia/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.eduardoinacio.javaspring_socialmedia.controller;
+
+import com.eduardoinacio.javaspring_socialmedia.entity.User;
+import com.eduardoinacio.javaspring_socialmedia.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+}
