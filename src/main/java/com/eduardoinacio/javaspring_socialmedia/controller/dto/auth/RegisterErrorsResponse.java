9c41b3e3e47a3cb0e1bc35c52adb237345e22db2
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-09 20:41:45 -0300

    Changing RegisterErrorsResponse to support a map of errors.

--- src/main/java/com/eduardoinacio/javaspring_socialmedia/controller/dto/auth/RegisterErrorsResponse.java
+++ src/main/java/com/eduardoinacio/javaspring_socialmedia/controller/dto/auth/RegisterErrorsResponse.java
@@ -1,6 +1,7 @@
 package com.eduardoinacio.javaspring_socialmedia.controller.dto.auth;
 
 import java.util.List;
+import java.util.Map;
 
-public record RegisterErrorsResponse(List<String> errors) {
+public record RegisterErrorsResponse(Map<String, List<String>> fieldErrors) {
 }
