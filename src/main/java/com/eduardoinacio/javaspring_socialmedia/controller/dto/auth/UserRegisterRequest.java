d6d3b50ec8f6425a91f974e525e052882866bea8
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-10 10:58:08 -0300

    Adding ValidDateOfBirth validation to UserRegisterRequest DTO.

--- src/main/java/com/eduardoinacio/javaspring_socialmedia/controller/dto/auth/UserRegisterRequest.java
+++ src/main/java/com/eduardoinacio/javaspring_socialmedia/controller/dto/auth/UserRegisterRequest.java
@@ -2,8 +2,17 @@ package com.eduardoinacio.javaspring_socialmedia.controller.dto.auth;
 
 import com.eduardoinacio.javaspring_socialmedia.controller.dto.auth.Annotations.StrongPassword;
 import com.eduardoinacio.javaspring_socialmedia.controller.dto.auth.Annotations.ValidCpfOrCnpj;
+import com.eduardoinacio.javaspring_socialmedia.controller.dto.auth.Annotations.ValidDateOfBirth;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
-public record UserRegisterRequest(@NotBlank @ValidCpfOrCnpj String document, @NotBlank String name, @NotBlank @Email String email, @NotBlank @StrongPassword String password) {
+import java.time.LocalDate;
+
+public record UserRegisterRequest(@NotBlank @ValidCpfOrCnpj String document,
+                                  @NotBlank String name,
+                                  @NotBlank @Email String email,
+                                  @NotNull @Past @ValidDateOfBirth(age = 18) LocalDate birthDate,
+                                  @NotBlank @StrongPassword String password) {
 }
