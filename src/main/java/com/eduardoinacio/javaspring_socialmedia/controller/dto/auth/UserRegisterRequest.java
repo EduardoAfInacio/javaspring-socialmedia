7dd53e641bde65018487d1b90d6e4ba33345e89c
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-10 00:13:23 -0300

    Adding StrongPassword Annotation in UserRegisterRequest DTO.

--- src/main/java/com/eduardoinacio/javaspring_socialmedia/controller/dto/auth/UserRegisterRequest.java
+++ src/main/java/com/eduardoinacio/javaspring_socialmedia/controller/dto/auth/UserRegisterRequest.java
@@ -1,7 +1,8 @@
 package com.eduardoinacio.javaspring_socialmedia.controller.dto.auth;
 
+import com.eduardoinacio.javaspring_socialmedia.controller.dto.auth.Annotations.StrongPassword;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record UserRegisterRequest(@NotBlank String name, @NotBlank @Email String email, @NotBlank String password) {
+public record UserRegisterRequest(@NotBlank String name, @NotBlank @Email String email, @NotBlank @StrongPassword String password) {
 }
