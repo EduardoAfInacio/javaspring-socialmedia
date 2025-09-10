1f1408b301b6fde1728e2883193b895334b66dfb
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-10 18:57:02 -0300

    Creating RedisService.

---
+++ src/main/java/com/eduardoinacio/javaspring_socialmedia/service/RedisService.java
@@ -0,0 +1,28 @@
+package com.eduardoinacio.javaspring_socialmedia.service;
+
+import com.eduardoinacio.javaspring_socialmedia.service.Mail.MailDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RedisService {
+    @Autowired
+    private RedisTemplate<String, MailDTO> redisTemplate;
+
+    private static final String EMAIL_QUEUE = "emailQueue";
+
+    public void pushEmail(MailDTO mail){
+        redisTemplate.opsForList().leftPush(EMAIL_QUEUE, mail);
+    }
+
+    public void popEmail(){
+        redisTemplate.opsForList().rightPop(EMAIL_QUEUE);
+    }
+
+    public List<MailDTO> getEmails(String to){
+        return redisTemplate.opsForList().range(EMAIL_QUEUE, 0, -1);
+    }
+}
