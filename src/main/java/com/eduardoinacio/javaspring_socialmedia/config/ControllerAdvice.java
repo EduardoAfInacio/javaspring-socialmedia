c13a83a84ce7e7ccc53295ae71d0c4f62d297005
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-10 13:29:45 -0300

    Creating exception handler to BadCretentialsException.

--- src/main/java/com/eduardoinacio/javaspring_socialmedia/config/ControllerAdvice.java
+++ src/main/java/com/eduardoinacio/javaspring_socialmedia/config/ControllerAdvice.java
@@ -3,6 +3,7 @@ package com.eduardoinacio.javaspring_socialmedia.config;
 import com.eduardoinacio.javaspring_socialmedia.controller.dto.auth.RegisterErrorsResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,5 +23,10 @@ public class ControllerAdvice {
             errors.computeIfAbsent(fieldError.getField(), k -> new ArrayList<>()).add(fieldError.getDefaultMessage());
         });
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new RegisterErrorsResponse(errors));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentialsException(BadCredentialsException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
